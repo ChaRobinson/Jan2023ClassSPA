af7dad083e277ca7daed4a652329729fbe1e2bfd
lilbigangel86 <lilbigangel86@yahoo.com>
2023-02-23 20:55:09 -0500

    23 Feb 23

---
+++ store/example.js
@@ -0,0 +1,45 @@
+// // function countVowels(word) {
+// //   let counter = 0;
+
+// //   for (let i = 0; i <= word.length; i++) {
+// //     switch (word.toLowerCase()[i]) {
+// //       case "a":
+// //       case "e":
+// //       case "i":
+// //       case "o":
+// //       case "u":
+// //         counter++;
+// //         break;
+// //     }
+// //   }
+// //   console.log(`The word '${word}' contains ${counter} vowels.`);
+// //   return counter;
+// // }
+
+// // console.log(countVowels("Ethiopia"));
+
+// function countLetters(small) {
+//   let counter = 0;
+//   for (let n = 0; n <= small.length; n++) {
+//     if (n == "a") {
+//       counter = small.length;
+//     }
+//     if (n == "e") {
+//       counter = small.length;
+//     }
+//     if (n == "i") {
+//       counter = small.length;
+//     }
+//     if (n == "o") {
+//       counter = small.length;
+//     }
+//     if (n == "u") {
+//       counter = small.length;
+//     }
+//     counter++;
+//     break;
+//   }
+//   return counter;
+// }
+
+// console.log(countLetters("Sweet"));
